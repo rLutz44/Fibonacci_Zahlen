fe2438161b19508ba25ed789778e287434377851
rLutz44 <robertlutz35@yahoo.de>
2021-10-02 19:10:41 +0200

    Update Fibonacci_Zahlen.py
    Code cleaning

--- Fibonacci_Zahlen.py
+++ Fibonacci_Zahlen.py
@@ -2,50 +2,60 @@ import numpy as np #numpy_arrays are faster in execution and need less space tha
 import random
 from datetime import datetime
 
-def fibonacci(fibonacciSequenzPosition, fibonacciSequenz):
+def fibonacci(fibonacci_sequenz_position: int, fibonacci_sequenz: int) -> str:
+    """
+    | Prints Fibonacci Number
     
-    if (type(fibonacciSequenzPosition) != int):
-        return "Invalid input, pls use positive integer numbers only \n"
-    if (fibonacciSequenzPosition < 0):
+    """
+    if (type(fibonacci_sequenz_position) != int):
         return "Invalid input, pls use positive integer numbers only \n"
     
-    if (fibonacciSequenzPosition == 0):
+    if (fibonacci_sequenz_position < 0):
+        return "Invalid input, pls use positive integer numbers only \n"
+    
+    if (fibonacci_sequenz_position == 0):
         return "Fibonacci Number for 0 is: 0 \n"
-    if (fibonacciSequenzPosition == 1 or fibonacciSequenzPosition == 2):
-        return "Fibonacci Number for"+str(fibonacciSequenzPosition)+" is: 1 \n"
     
-    fibonacciSequenzLastIndex = fibonacciSequenz.size-1
-    if (fibonacciSequenzPosition > fibonacciSequenzLastIndex):
-        fibonacciSequenz = fibonacciSequenzExtender(fibonacciSequenzPosition, fibonacciSequenzLastIndex ,fibonacciSequenz)
+    if (fibonacci_sequenz_position == 1 or fibonacci_sequenz_position == 2):
+        return "Fibonacci Number for"+str(fibonacci_sequenz_position)+" is: 1 \n"
     
-    return "Fibonacci Number for "+str(fibonacciSequenzPosition)+" is: "+str(fibonacciSequenz[fibonacciSequenzPosition])+"\n" 
+    fibonacci_sequenz_last_index: int = fibonacci_sequenz.size-1
+    if (fibonacci_sequenz_position > fibonacci_sequenz_last_index):
+        fibonacci_sequenz = fibonacci_sequenz_extender(fibonacci_sequenz_position, fibonacci_sequenz_last_index, fibonacci_sequenz)
+    
+    return "Fibonacci Number for "+str(fibonacci_sequenz_position)+" is: "+str(fibonacci_sequenz[fibonacci_sequenz_position])+"\n" 
 
 
-def fibonacciSequenzExtender(fibonacciSequenzPosition, fibonacciSequenzLastIndex ,fibonacciSequenz):
+def fibonacci_sequenz_extender(fibonacci_sequenz_position: int, fibonacci_sequenz_last_index: int, fibonacci_sequenz: int) -> np:
+    """
+    | Extends the sequenz to the current needed Position, if Position > 3
     
-    while(fibonacciSequenzPosition > fibonacciSequenzLastIndex):
-        fibonacciSequenzLastNumber = fibonacciSequenz[fibonacciSequenzLastIndex]
-        fibonacciSequenzSecondLastNumber = fibonacciSequenz[fibonacciSequenzLastIndex-1]
-        fibonacciSequenzNewNumber = fibonacciSequenzLastNumber + fibonacciSequenzSecondLastNumber
-        fibonacciSequenz = np.append(fibonacciSequenz, [fibonacciSequenzNewNumber])
-        fibonacciSequenzLastIndex = fibonacciSequenz.size-1
+    """
+    while(fibonacci_sequenz_position > fibonacci_sequenz_last_index):
+        fibonacci_sequenzLastNumber: int         = fibonacci_sequenz[fibonacci_sequenz_last_index]
+        fibonacci_sequenzSecondLastNumber: int   = fibonacci_sequenz[fibonacci_sequenz_last_index-1]
+        fibonacci_sequenzNewNumber: int          = fibonacci_sequenzLastNumber + fibonacci_sequenzSecondLastNumber
+        fibonacci_sequenz                        = np.append(fibonacci_sequenz, [fibonacci_sequenzNewNumber])
+        fibonacci_sequenz_last_index             = fibonacci_sequenz.size-1
         
-    return fibonacciSequenz
+    return fibonacci_sequenz
         
 
 #--------------------Main -----------------------#
 
-startTime = datetime.now()
-fibonacciSequenz = np.ulonglong([0, 1, 1])
-
-#With random number generator
-loopStart = 0
-loopEnd = 523
-fibonacciFirstPosition = 0 
-fibonacciMaxPosition = 93 #93 Last Position before overflow
-
-for randomNumber in range(loopStart, loopEnd):
-    fibonacciSequenzPosition = random.randint(fibonacciFirstPosition, fibonacciMaxPosition)
-    print(fibonacci(fibonacciSequenzPosition, fibonacciSequenz)) 
-
-print("Execution time: " +str(datetime.now()-startTime))
+if (__name__ == "__main__"):
+    
+    start_time                      = datetime.now()
+    fibonacci_sequenz               = np.ulonglong([0, 1, 1])
+    
+    #With random number generator
+    loop_start: int                 = 0
+    loop_end: int                   = 523
+    fibonacci_first_position: int   = 0 
+    fibonacci_max_position: int     = 93 #93 Last Position before overflow
+    
+    for random_number in range(loop_start, loop_end):
+        fibonacci_sequenz_position: int = random.randint(fibonacci_first_position, fibonacci_max_position)
+        print(fibonacci(fibonacci_sequenz_position, fibonacci_sequenz)) 
+    
+    print("Execution time: " +str(datetime.now()-start_time))
